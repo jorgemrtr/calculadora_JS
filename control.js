849e6353f2ec679c2e1cacfcc9f21dfbe6c8fb33
daw <daw>
2019-11-20 20:55:57 +0100

    fin regex

--- control.js
+++ control.js
@@ -1,13 +1,10 @@
 //function analizarInput(valorIntroducido) {
-
-    var cadenaIntroducida = "-1116.53-";
     /*OPEACIONES SIMPLES
        - Resta
        + Suma
        * Multiplicacion
        / Division
-    */
-    /*OPEACIONES COMPLEJAS:
+    OPEACIONES COMPLEJAS:
     Constantes
         PI - Num PI
         E - Num E
@@ -23,33 +20,33 @@
         SQRT (Función) Raíz cuadrada de un número.
         RAIZ N (Función) Raíz n-esima de un número, Ejemplo Raíz cúbica, Raíz Cuarta...
      */
-    //PREGUNTAR SI HAY QUE HACER:INVERSIÓN DE SIGNO (POSITIVO/NEGATIVO) +-s
 
     //REGEX OPERANDOS
-    var estructuraOperando = /-?[0-9]+\.?[0-9]+/;
+    var estructuraOperando = /-?[0-9]+\.?[0-9]+|^PI|E/;
 
     //REGEX OPERACIONES
     //simples
-    var estructuraOperacionSimple = /^-|\+|\*|\/?/;
+    //var estructuraOperacionSimple = /^-|\+|\*|\/?/;
     //complejas
-    var estructuraOperacionCompleja = /^PI|E|\^|ABS|SEN|COS|TAN|LOG10|LOG2|LN|SQRT|RAIZ?/;
+    //var estructuraOperacionCompleja = /^\^|ABS|SEN|COS|TAN|LOG10|LOG2|LN|SQRT|RAIZ?/;
+    //total
+    //funcion EVAL*****************
+    var estructuraOperaciones = /^-|\+|\*|\/|\^|ABS|SEN|COS|TAN|LOG10|LOG2|LN|SQRT|RAIZ?/;
 
     /*Recorremos la cadena introducida en orden Operando > Operacion > Operando, si el orden no es valido dara error*/
-
-            document.write(cadenaIntroducida);
+    var cadenaIntroducida = "PI-1116.53-89";
+            document.write("Cadena: " + cadenaIntroducida);
             document.write("<br>");
 
-            var operando = cadenaIntroducida.match(estructuraOperando);
-            document.write("operando = " + operando);
+            var operando = cadenaIntroducida.match(estructuraOperando).toString();
+            document.write("operando: " + operando);
             document.write("<br>");
             cadenaIntroducida = cadenaIntroducida.substring(operando.length, cadenaIntroducida.length);
 
-            var operacionBas = cadenaIntroducida.match(estructuraOperacionSimple);
-            var operacionComp = cadenaIntroducida.match(estructuraOperacionCompleja);
-            document.write("operacion = " + operacionBas);
+            var operacion = cadenaIntroducida.match(estructuraOperaciones).toString();
+            document.write("operacion: " + operacion);
             document.write("<br>");
-            document.write("operacion = " + operacionComp);
-            document.write("<br>");
+
 
 
 
