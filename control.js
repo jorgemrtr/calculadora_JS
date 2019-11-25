bedaf1c94ca134c923f6c359197e1d96542dd0e0
casa <casa@dev>
2019-11-25 14:17:14 +0100

    funcion con input desde html

--- control.js
+++ control.js
@@ -1,53 +1,63 @@
-//function analizarInput(valorIntroducido) {
-    /*OPEACIONES SIMPLES
-       - Resta
-       + Suma
-       * Multiplicacion
-       / Division
-    OPEACIONES COMPLEJAS:
-    Constantes
-        PI - Num PI
-        E - Num E
-    Funciones
-        ^ - Num elevado a otro
-        ABS - Valor absoluto de un num
-        SEN - Valor del seno de un ángulo (Radianes).
-        COS - Valor del coseno de un ángulo (Radianes).
-        TAN - Valor de la tangente de un ángulo (radianes)
-        LOG10 - Logaritmo en Base 10 de un número.
-        LOG2 - logaritmo en Base 2 de un número.
-        LN - logaritmo Natural de un número.
-        SQRT (Función) Raíz cuadrada de un número.
-        RAIZ N (Función) Raíz n-esima de un número, Ejemplo Raíz cúbica, Raíz Cuarta...
-     */
+/*OPEACIONES SIMPLES
+	- Resta
+	+ Suma
+	* Multiplicacion
+	/ Division
+OPEACIONES COMPLEJAS:
+Constantes
+	PI - Num PI
+	E - Num E
+Funciones
+	^ - Num elevado a otro
+	ABS - Valor absoluto de un num
+	SEN - Valor del seno de un ángulo (Radianes).
+	COS - Valor del coseno de un ángulo (Radianes).
+	TAN - Valor de la tangente de un ángulo (radianes)
+	LOG10 - Logaritmo en Base 10 de un número.
+	LOG2 - logaritmo en Base 2 de un número.
+	LN - logaritmo Natural de un número.
+	SQRT (Función) Raíz cuadrada de un número.
+	RAIZ N (Función) Raíz n-esima de un número, Ejemplo Raíz cúbica, Raíz Cuarta...
+ */
 
-    //REGEX OPERANDOS
-    var estructuraOperando = /-?[0-9]+\.?[0-9]+|^PI|E/;
+function calculadora(valorIntroducido) {
+	//REGEX OPERANDOS
+	var estructuraOperando = /^-?[0-9]+\.?[0-9]+|[0-9]|^PI|E/;
+	//var estructuraOperando = /^-?[0-9]+(\.[0-9])|^PI|E/;
+	//REGEX OPERACIONES
+	var estructuraOperaciones = /^-|\+|\*|\/|\^|ABS|SEN|COS|TAN|LOG10|LOG2|LN|SQRT|RAIZ?/;
+	//funcion EVAL*****************
 
-    //REGEX OPERACIONES
-    //simples
-    //var estructuraOperacionSimple = /^-|\+|\*|\/?/;
-    //complejas
-    //var estructuraOperacionCompleja = /^\^|ABS|SEN|COS|TAN|LOG10|LOG2|LN|SQRT|RAIZ?/;
-    //total
-    //funcion EVAL*****************
-    var estructuraOperaciones = /^-|\+|\*|\/|\^|ABS|SEN|COS|TAN|LOG10|LOG2|LN|SQRT|RAIZ?/;
+	/*Recorremos la cadena introducida en orden Operando > Operacion > Operando, si el orden no es valido dara error*/
 
-    /*Recorremos la cadena introducida en orden Operando > Operacion > Operando, si el orden no es valido dara error*/
-    var cadenaIntroducida = "PI-1116.53-89";
-            document.write("Cadena: " + cadenaIntroducida);
-            document.write("<br>");
+	//asignamos la cadena recibida a una variable
+	var cadenaIntroducida = valorIntroducido;
+	document.write("Cadena: " + cadenaIntroducida + "<br>");
 
-            var operando = cadenaIntroducida.match(estructuraOperando).toString();
-            document.write("operando: " + operando);
-            document.write("<br>");
-            cadenaIntroducida = cadenaIntroducida.substring(operando.length, cadenaIntroducida.length);
+	//primer operador
+	var operando1 = cadenaIntroducida.match(estructuraOperando);
+	if (operando1 == null) {
+		document.write('error operando1<br>');
+	} else {
+		document.write("operando1: " + operando1 + "<br>");
+		cadenaIntroducida = cadenaIntroducida.substring(operando1.toString().length, cadenaIntroducida.length);
+	}
 
-            var operacion = cadenaIntroducida.match(estructuraOperaciones).toString();
-            document.write("operacion: " + operacion);
-            document.write("<br>");
+	//operacion
+	var operacion = cadenaIntroducida.match(estructuraOperaciones);
+	if (operacion == null) {
+		document.write('error operacion<br>');
+	} else {
+		document.write("operacion: " + operacion + "<br>");
+		cadenaIntroducida = cadenaIntroducida.substring(operacion.toString().length, cadenaIntroducida.length);
+	}
 
-
-
-
-//}
+	//segundo operador
+	var operando2 = cadenaIntroducida.match(estructuraOperando);
+	if (operando2 == null) {
+		document.write('error operando2<br>');
+	} else {
+		document.write("operando2: " + operando2 + "<br>");
+		cadenaIntroducida = cadenaIntroducida.substring(operando2.toString().length, cadenaIntroducida.length);
+	}
+}
